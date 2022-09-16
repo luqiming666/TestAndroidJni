2e575f6b46c6cfe88f797209901ef9ea895dc0a1
Qiming <LuQiMing@aactechnologies.com>
2022-09-16 22:02:16 +0800

    The first commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,33 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_testandroidjni_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT jint JNICALL
+Java_com_example_testandroidjni_MainActivity_addTwoNumbers(
+        JNIEnv* env,
+        jobject /* this */,
+        jint num1,
+        jint num2) {
+    return num1+num2;
+}
+
+extern "C" JNIEXPORT jint JNICALL
+Java_com_example_testandroidjni_MainActivity_testLength(
+        JNIEnv* env,
+        jobject /* this */,
+        jstring text) {
+    char* str = (char*) env->GetStringUTFChars(text,JNI_FALSE);
+    if (str != NULL) {
+        size_t len = std::strlen(str);
+        env->ReleaseStringUTFChars(text, str);
+        return len;
+    }
+    return 0;
+}
